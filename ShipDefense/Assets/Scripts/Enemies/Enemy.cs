a550342c7358f338d04eb3e55ed4f8c776d7dc9c
ZhengxiZ <86876575+Jessonzzx@users.noreply.github.com>
2025-06-10 21:22:46 -0600

    Added the ark, fix the tilemap.

--- ShipDefense/Assets/Scripts/Enemies/Enemy.cs
+++ ShipDefense/Assets/Scripts/Enemies/Enemy.cs
@@ -31,6 +31,10 @@ public class Enemy : MonoBehaviour
     {
         Chaser();
     }
+    protected virtual void FixedUpdate()
+    {
+        Chaser();
+    }
 
     protected virtual void Chaser()
     {
