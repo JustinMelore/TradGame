40f654d53e0fedaf82047fe63513d44909bb2e53
JustinMelore <melorejustin@gmail.com>
2025-07-09 14:13:57 -0600

    Added animations to the health bars of the player and ship

--- ShipDefense/Assets/Scripts/UI/PlayerHealthUI.cs
+++ ShipDefense/Assets/Scripts/UI/PlayerHealthUI.cs
@@ -4,9 +4,13 @@ using UnityEngine.UI;
 public class PlayerHealthUI : MonoBehaviour
 {
     [SerializeField] private Image healthBarFill;
+    [SerializeField] private Animator animator;
 
     public void SetHealth(float current, float max)
     {
-        healthBarFill.fillAmount = current / max;
+        float newHealthFill = current / max;
+        if (newHealthFill > healthBarFill.fillAmount) animator.SetTrigger("Healed");
+        else if (newHealthFill < healthBarFill.fillAmount) animator.SetTrigger("Damaged");
+            healthBarFill.fillAmount = newHealthFill;
     }
 }
