819a70426e1922783616d4c83ad9ca28d2ce3aec
JustinMelore <melorejustin@gmail.com>
2025-07-10 14:50:35 -0600

    Fixed bug that allowed enemies to be hit twice by the same attack

--- ShipDefense/Assets/Scripts/Player/PlayerAttack/PlayerHurtBox.cs
+++ ShipDefense/Assets/Scripts/Player/PlayerAttack/PlayerHurtBox.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -5,17 +6,25 @@ using UnityEngine;
 /// </summary>
 public class PlayerHurtbox : Hurtbox
 {
+    private HashSet<Enemy> damagedEnemies = new HashSet<Enemy>();
+    
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
             Enemy enemy = collision.GetComponentInParent<Enemy>();
             Debug.Log(enemy);
-            if (enemy != null)
+            if (enemy != null && !damagedEnemies.Contains(enemy))
             {
                 enemy.DamageEnemy(damage);
+                damagedEnemies.Add(enemy);
                 Debug.Log("Player hit enemy!");
             }
         }
+    }
+
+    protected void OnEnable()
+    {
+        damagedEnemies.Clear();
     }
 }
