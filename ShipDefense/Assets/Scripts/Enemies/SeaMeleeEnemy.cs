4c539e9f98739714440aac0e6e8f5fecc224546e
ZhengxiZ <86876575+Jessonzzx@users.noreply.github.com>
2025-07-16 18:42:34 -0600

    Update SeaMeleeEnemy.cs

--- ShipDefense/Assets/Scripts/Enemies/SeaMeleeEnemy.cs
+++ ShipDefense/Assets/Scripts/Enemies/SeaMeleeEnemy.cs
@@ -46,7 +46,6 @@ public class SeaMeleeEnemy : MeleeEnemy
         canParry = false;
         hurtbox.Deactivate();
     }
-    
     public void MakeVulnerable()
     {
         enemyHitbox.SetActive(true);
